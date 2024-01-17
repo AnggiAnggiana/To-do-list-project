add6fa1a7c9e3296d461c104da664e19ce833ed2
AnggiAnggianaSujana <anggiana0092@gmail.com>
2024-01-17 21:42:45 +0700

    create tables

--- todo/static/js/deadline-timer.js
+++ todo/static/js/deadline-timer.js
@@ -1,6 +1,6 @@
-document.querySelectorAll('.content-container').forEach(contentContainer => {
-    const deadline = contentContainer.querySelector('#deadline')
-    const importantTaskDeadline = contentContainer.querySelector('#importantTaskDeadline')
+document.querySelectorAll('.urgent').forEach(urgent => {
+    const deadline = urgent.querySelector('#deadline')
+    const importantTaskDeadline = urgent.querySelector('#importantTaskDeadline')
 
     console.log(deadline.textContent)
     const deadlineDate = Date.parse(deadline.textContent)
@@ -14,10 +14,16 @@ document.querySelectorAll('.content-container').forEach(contentContainer => {
         const minutes = Math.floor((deadlineDate / (1000 * 60) - (now / (1000 * 60))) % 60)
         const seconds = Math.floor((deadlineDate / (1000) - (now / (1000))) % 60)
 
-        if (difference <= 0) {
+        if (difference < 0) {
             importantTaskDeadline.innerHTML = "Deadline expired"
-        } else if (difference > 0) {
+        } else if (difference > 0 && days >= 1) {
             importantTaskDeadline.innerHTML = days + " hari " + hours + " jam " + minutes + " menit " + seconds + " detik "
+        } else if (difference > 0 && hours >= 1) {
+            importantTaskDeadline.innerHTML = hours + " jam " + minutes + " menit " + seconds + " detik "
+        } else if (difference > 0 && minutes >= 1) {
+            importantTaskDeadline.innerHTML = minutes + " menit " + seconds + " detik "
+        } else {
+            importantTaskDeadline.innerHTML = seconds + " detik "
         }
     }, 1000)
 })
